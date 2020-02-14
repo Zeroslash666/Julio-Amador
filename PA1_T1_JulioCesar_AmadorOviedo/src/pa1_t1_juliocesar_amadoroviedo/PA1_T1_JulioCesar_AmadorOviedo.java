fb418799c2bfb76147f96f9a38f12bd9ecc90514
Zeroslash666 <ja43793666@gmail.com>
2020-02-14 02:11:07 -0600

    Estructura del Proyecto Creada

---
+++ PA1_T1_JulioCesar_AmadorOviedo/src/pa1_t1_juliocesar_amadoroviedo/PA1_T1_JulioCesar_AmadorOviedo.java
@@ -0,0 +1,21 @@
+
+package pa1_t1_juliocesar_amadoroviedo;
+
+
+public class PA1_T1_JulioCesar_AmadorOviedo {
+
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Observando lo hecho");
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
